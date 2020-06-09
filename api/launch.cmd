2d81e289d7b1e6d0df4a63bbb840c047c3ad66d5
Dimitri K. Sifoua <dimitri.sifoua@gmail.com>
2020-06-09 22:43:34 +0000

    Update README.md

---
+++ api/launch.cmd
@@ -0,0 +1,8 @@
+set TFIDF_VECTORIZER=src/dumps/tfidf_vectorizer.pkl
+set INTENT_RECOGNIZER=src/dumps/intent_recognizer.pkl
+set TAG_CLASSIFIER=src/dumps/tag_classifier.pkl
+
+set FLASK_APP=main.py
+set FLASK_ENV=development
+
+flask run --host=0.0.0.0
